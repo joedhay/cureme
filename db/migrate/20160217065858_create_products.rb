0ad7bbf70d355c4eb185bf4abdfe84f429173ac9
Jerico Lopez <jerico.lopez@glei.de>
2016-03-05 08:30:22 +0800

    design

--- db/migrate/20160217065858_create_products.rb
+++ db/migrate/20160217065858_create_products.rb
@@ -1,11 +1,11 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.integer :category_id
+        t.integer :category_id
       t.string :item_name
       t.decimal :price
       t.string :description
-      t.string :groups
+      t.string :groupssearch_by_category
 
       t.timestamps null: false
     end
