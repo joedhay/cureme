5fc1b24d8453205d2ba674788daef07a5b232fbb
root <joener.preagola@glei.de>
2016-03-11 15:57:05 +0800

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #before_filter :set_categories
-  before_filter :get_categories,  :except => [:login]
-  before_filter :get_user_image,  :except => [:login,:register]
+  #before_filter :get_categories,  :except => [:login]
+  #before_filter :get_user_image,  :except => [:login,:register]
 
   def get_categories
       @categories = Category.all
