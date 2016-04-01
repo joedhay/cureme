6d85be758ad8ff83c513d8e14f62707669043e51
Jerico Lopez <jerico.lopez1993@gmail.com>
2016-04-01 11:00:34 +0800

    Designs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_filter :get_categories
   before_filter :get_user_image,  :except => [:login,:register]
 
   def get_categories
       @categories = Category.all
-
-  end
-
-  def set_categories
-    if session[:categories].present?
-      session[:categories] = nil
-    end
   end
 
   def get_user_image
